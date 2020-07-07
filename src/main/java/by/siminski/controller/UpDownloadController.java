2e54443682b26c39d136d3a3f9bfff875efeb70c
Alex <32672890+ALXSIMINSKI@users.noreply.github.com>
2020-07-08 00:34:10 +0300

    uploading files

--- src/main/java/by/siminski/controller/UpDownloadController.java
+++ src/main/java/by/siminski/controller/UpDownloadController.java
@@ -8,51 +8,74 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 @Controller
-public class DownloadController {
+public class UpDownloadController {
 
     private static final String DEFAULT_PRICE_LIST_FILE_NAME = "prices.doc";
     private static final String DEFAULT_AGREEMENT_FILE_NAME = "agreement.doc";
-    private static final String DIRECTORY_TO_DOWNLOAD = "/download";
+    private static final String DIRECTORY_TO_UP_DOWNLOAD = "/download/";
 
     @Autowired
     private ServletContext servletContext;
 
+    @PostMapping("/upload/prices")
+    public String uploadPrices(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("p_status",uploadFile(file, DEFAULT_PRICE_LIST_FILE_NAME));
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/upload/agreement")
+    public String uploadAgreement(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("a_status", uploadFile(file, DEFAULT_AGREEMENT_FILE_NAME));
+        return "redirect:/admin";
+    }
+
     @GetMapping("/download/pl")
-    public ResponseEntity<InputStreamResource> downloadPriceList(
-            @RequestParam(defaultValue = DEFAULT_PRICE_LIST_FILE_NAME) String fileName, Model model) throws IOException {
+    public ResponseEntity<InputStreamResource> downloadPriceList(@RequestParam(defaultValue = DEFAULT_PRICE_LIST_FILE_NAME) String fileName) {
         try {
             return buildResponseOnDownload(fileName);
         } catch (IOException e) {
-            model.addAttribute("Error");
             return null;
         }
     }
 
     @GetMapping("/download/agreement")
-    public ResponseEntity<InputStreamResource> downloadAgreement(
-            @RequestParam(defaultValue = DEFAULT_AGREEMENT_FILE_NAME) String fileName,  Model model) throws IOException {
+    public ResponseEntity<InputStreamResource> downloadAgreement(@RequestParam(defaultValue = DEFAULT_AGREEMENT_FILE_NAME) String fileName) {
         try {
             return buildResponseOnDownload(fileName);
         } catch (IOException e) {
-            model.addAttribute("Error");
             return null;
+        }
+    }
+
+    private String uploadFile(MultipartFile file, String fileName) {
+        try {
+            Path path = new ClassPathResource(DIRECTORY_TO_UP_DOWNLOAD + fileName).getFile().toPath();
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            return "OK";
+        } catch (IOException e) {
+            return "ERROR";
         }
     }
 
     private ResponseEntity<InputStreamResource> buildResponseOnDownload(String fileName) throws IOException {
         MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(this.servletContext, fileName);
 
-        File file = new ClassPathResource(DIRECTORY_TO_DOWNLOAD + "/" + fileName).getFile();
+        File file = new ClassPathResource(DIRECTORY_TO_UP_DOWNLOAD + fileName).getFile();
         InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
         return ResponseEntity.ok()
