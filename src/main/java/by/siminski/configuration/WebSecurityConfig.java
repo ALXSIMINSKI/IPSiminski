569c8ba53aac304fbd60f94891b70817e2b9c838
Alex <32672890+ALXSIMINSKI@users.noreply.github.com>
2020-08-06 23:02:03 +0300

    removed commented antMatchers

--- src/main/java/by/siminski/configuration/WebSecurityConfig.java
+++ src/main/java/by/siminski/configuration/WebSecurityConfig.java
@@ -56,7 +56,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/registration").not().fullyAuthenticated()
                 .antMatchers("/settings/**", "/requests/**", "/upload/**, /catalog/**").hasRole("ADMIN")
-//                .antMatchers("/resources/**").permitAll()
                 .anyRequest().permitAll()
                 .and().formLogin().loginPage("/login").defaultSuccessUrl("/welcome", true)
                 .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login").permitAll();
